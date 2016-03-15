56e3689cd30aa47fac67028b8f685715ba7ac505
yutuo <yutuo5@gmail.com>
2016-03-15 14:31:16 +0900

    开发中

---
+++ Gulpfile.js
@@ -0,0 +1,142 @@
+"use strict";
+
+var os = require("os");
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var sass = require("gulp-ruby-sass");
+var jshint = require("gulp-jshint");
+var uglify = require("gulp-uglifyjs");
+var rename = require("gulp-rename");
+var concat = require("gulp-concat");
+var notify = require("gulp-notify");
+var header = require("gulp-header");
+var minifycss = require("gulp-minify-css");
+//var jsdoc        = require("gulp-jsdoc");
+//var jsdoc2md     = require("gulp-jsdoc-to-markdown");
+var pkg = require("./package.json");
+var dateFormat = require("dateformatter").format;
+var replace = require("gulp-replace");
+
+pkg.name = "MdEditor.yt";
+pkg.today = dateFormat;
+
+var headerComment = ["/*",
+    " * <%= pkg.name %>",
+    " *",
+    " * @file        <%= fileName(file) %> ",
+    " * @version     v<%= pkg.version %> ",
+    " * @description <%= pkg.description %>",
+    " * @license     MIT License",
+    " * @author      <%= pkg.author %>",
+    " * {@link       <%= pkg.homepage %>}",
+    " */",
+    "\r\n"].join("\r\n");
+
+var headerMiniComment = "/*! <%= pkg.name %> v<%= pkg.version %> | <%= fileName(file) %> | <%= pkg.description %> | MIT License | By: <%= pkg.author %> | <%= pkg.homepage %> | <%=pkg.today('Y-m-d') %> */\r\n";
+
+var dist = 'dist';
+
+var cssSrcs = [
+    'lib/markdownyt/markdownyt.css',
+    'css/mdeditoryt.css',
+];
+
+gulp.task("css", function () {
+    return gulp.src(cssSrcs)
+        .pipe(concat("mdeditoryt.css"))
+        .pipe(gulp.dest(dist))
+        .pipe(concat("mdeditoryt.min.css"))
+        .pipe(minifycss({compatibility: 'ie8'}))
+        .pipe(gulp.dest(dist))
+        .pipe(notify({message: "MdEditorYt css task complete!"}));
+});
+
+var jsSrcs = [
+    'lib/markdownyt/markdownyt.js',
+    'src/mdeditoryt.js',
+];
+
+gulp.task("js", function () {
+    return gulp.src(jsSrcs)
+        .pipe(concat("mdeditoryt.js"))
+        .pipe(gulp.dest(dist))
+        .pipe(concat("mdeditoryt.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest(dist))
+        .pipe(header(headerMiniComment, {
+            pkg: pkg, fileName: function (file) {
+                var name = file.path.split(file.base + "\\");
+                return (name[1] ? name[1] : name[0]).replace(/\\/g, "");
+            }
+        }))
+        .pipe(gulp.dest(dist))
+        .pipe(notify({message: "MdEditorYt js task complete!"}));
+});
+
+var cmPath = 'lib/codemirror';
+var cmJsSrcs = [
+    cmPath + "/lib/codemirror.js",
+    cmPath + "/addon/mode/simple.js",
+    cmPath + "/addon/mode/overlay.js",
+    cmPath + "/addon/mode/loadmode.js",
+    cmPath + "/addon/mode/multiplex.js",
+    cmPath + "/addon/runmode/runmode.js",
+    cmPath + "/mode/meta.js",
+    cmPath + "/mode/markdown/markdown.js",
+    cmPath + "/addon/edit/trailingspace.js",
+    cmPath + "/addon/dialog/dialog.js",
+    cmPath + "/addon/search/searchcursor.js",
+    cmPath + "/addon/search/search.js",
+    cmPath + "/addon/scroll/annotatescrollbar.js",
+    cmPath + "/addon/search/matchesonscrollbar.js",
+    cmPath + "/addon/display/placeholder.js",
+    cmPath + "/addon/edit/closetag.js",
+    cmPath + "/addon/fold/foldcode.js",
+    cmPath + "/addon/fold/foldgutter.js",
+    cmPath + "/addon/fold/indent-fold.js",
+    cmPath + "/addon/fold/brace-fold.js",
+    cmPath + "/addon/fold/xml-fold.js",
+    cmPath + "/addon/fold/markdown-fold.js",
+    cmPath + "/addon/fold/comment-fold.js",
+    cmPath + "/addon/selection/active-line.js",
+    cmPath + "/addon/edit/closebrackets.js",
+    cmPath + "/addon/display/fullscreen.js",
+    cmPath + "/addon/search/match-highlighter.js"
+];
+
+gulp.task("cm-css", function () {
+
+    var csses = [cmPath + "/lib/codemirror.css"];
+
+    return gulp.src(csses)
+        .pipe(concat("codemirror.min.css"))
+        .pipe(gulp.dest(cmPath))
+        .pipe(minifycss({compatibility: 'ie8'}))
+        .pipe(gulp.dest(cmPath))
+        .pipe(notify({message: "codemirror css task complete!"}));
+});
+
+gulp.task("cm-js", function () {
+
+    return gulp.src(cmJsSrcs)
+        .pipe(concat("codemirror.min.js"))
+        .pipe(gulp.dest(cmPath))
+        .pipe(uglify())
+        .pipe(gulp.dest(cmPath))
+        .pipe(header(headerMiniComment, {
+            pkg: pkg, fileName: function (file) {
+                var name = file.path.split(file.base + "\\");
+                return (name[1] ? name[1] : name[0]).replace(/\\/g, "");
+            }
+        }))
+        .pipe(gulp.dest(cmPath))
+        .pipe(notify({message: "codemirror task complete!"}));
+});
+
+
+gulp.task("default", function () {
+    gulp.run("css");
+    gulp.run("js");
+    gulp.run("cm-css");
+    gulp.run("cm-js");
+});
--- src/mdeditoryt.js
+++ src/mdeditoryt.js
@@ -3,18 +3,151 @@
 
 'use strict';
 
+var defaults = {
+    html:           true,        // Enable HTML tags in source
+    xhtmlOut:       true,        // Use '/' to close single tags (<br />)
+    breaks:         true,        // Convert '\n' in paragraphs into <br>
+    linkify:        true,         // autoconvert URL-like texts to links
+    typographer:    false,         // Enable smartypants and other sweet transforms
+
+
+    useAbbr:        true,
+    useContainer:   true,
+    useDeflist:     true,
+    useEmoji:       true,
+    useFootnote:    true,
+    useIns:         true,
+    useMark:        true,
+    useSub:         true,
+    useSup:         true,
+    useToc:         true,
+    useMath:        false,
+
+    useLinkNewWin:  false,
+    useSourceLine:  false,
+    useCodeBlockPre:false,
+    
+    highlight:      true,
+    useLineNumber:  true,
+    useShowLangName:true,
+    
+    editorTheme: 'default',
+    tabSize: 4,
+    dragDrop: false,
+    lineNumbers: true,
+    lineWrapping: true,
+    autoFocus: true,
+    autoCloseTags: true,
+    readOnly: false,
+    indentUnit: 4,
+    value: '',
+    
+    delay: 300,
+    searchReplace: true,
+    syncScrolling: true,
+    mdValueName: '',
+    htmlValueName: '',
+    
+};
+
+            
+var timer;
 var MdEditorYt = function() {};
 
 MdEditorYt.prototype = {
-    init: function () {
+    options: {},
+    init: function (id, settingOptions) {
+        this.options = $.extend(true, defaults, settingOptions);
+        // Editor的设置
+        this.editor = (typeof id === "object") ? $(id) : $("#" + id);
+        this.editor.addClass('MdEditorYt');
+        this.editor.css({
+            width: (typeof this.options.width === "number") ? this.options.width + "px" : this.options.width,
+            height: (typeof this.options.height === "number") ? this.options.height + "px" : this.options.height
+        });
         
+        this.previewContainer = $('<div class="previewContainer"></div>');
+        this.editor.append(this.previewContainer);
+        
+        this.mdValue = $('<textarea class="mdValue" name="' + this.options.mdValueName + '"></textarea>');
+        this.editor.append(this.mdValue);
+        
+        this.htmlValue = $('<textarea class="htmlValue" name="' + this.options.htmlValueName + '"></textarea>');
+        this.editor.append(this.htmlValue);
+        
+        // CodeMirror的设置
+        this.cmContainer = $('<div class="cmContainer"></div>');
+        this.editor.append(this.cmContainer);
+        
+        var codeMirrorConfig = {
+            mode: 'markdown',
+            theme: this.options.editorTheme,
+            tabSize: this.options.tabSize,
+            dragDrop: this.options.dragDrop,
+            autofocus: this.options.autoFocus,
+            autoCloseTags: this.options.autoCloseTags,
+            readOnly: (this.options.readOnly) ? "nocursor" : false,
+            indentUnit: this.options.indentUnit,
+            lineNumbers: this.options.lineNumbers,
+            lineWrapping: this.options.lineWrapping,
+            value: this.options.value,
+            // extraKeys: {
+            //     "Ctrl-Q": function (cm) {
+            //         cm.foldCode(cm.getCursor());
+            //     }
+            // },
+            // foldGutter: settings.codeFold,
+            // gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+            // matchBrackets: settings.matchBrackets,
+            // indentWithTabs: settings.indentWithTabs,
+            // styleActiveLine: settings.styleActiveLine,
+            // styleSelectedText: settings.styleSelectedText,
+            // autoCloseBrackets: settings.autoCloseBrackets,
+            // showTrailingSpace: settings.showTrailingSpace,
+            // highlightSelectionMatches: ( (!settings.matchWordHighlight) ? false : {showToken: (settings.matchWordHighlight === "onselected") ? false : /\w/} )
+        };
+        
+        this.cmEditor = new CodeMirror(this.cmContainer.get(0), codeMirrorConfig);
+        this.markdownYt = new MarkdownYt(this.options);
+        
+        this.save();
+        this.bindChangeEvent();
+        
+        return this;
     },
+    
+    bindChangeEvent: function () {
+        var _this = this;
+        this.cmEditor.on("change", function () {
+            timer = setTimeout(function () {
+                clearTimeout(timer);
+                _this.save();
+                timer = null;
+            }, _this.options.delay);
+        });
+        return this;
+    },
+    
+    save: function() {
+        if (timer === null) {
+            return this;
+        }
+        
+        var mdValue = this.cmEditor.getValue();
+        this.mdValue.val(mdValue);
+        
+        var htmlValue = this.markdownYt.render(mdValue);
+        this.htmlValue.val(htmlValue);
+        
+        this.previewContainer.html(htmlValue);
+    }
     
     
 } 
 
-module.exports = function(settingOptions) {
+module.exports = function(id, settingOptions) {
     var mdEditorYt = new MdEditorYt();
+    mdEditorYt.init(id, settingOptions);
     return mdEditorYt;
 };
 
